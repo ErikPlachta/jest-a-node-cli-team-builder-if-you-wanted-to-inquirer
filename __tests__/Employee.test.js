5fddddece46777f5acd15205363b9206276c261d
Erik Plachta <eplachta@gmail.com>
2022-01-15 16:36:00 -0500

    Add failing tests and confirmed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,9 +5,43 @@ const Employee = require('../lib/Employee');
 
 //-- Creating an Employee
 test('creates a new employee', () => {
+    
+    //-- make employee to test
     const employee = new Employee("Erik",'001',"erik@noemail.com");
 
+    //-- what it should be
     expect(employee.name).toBe('Erik');
     expect(employee.id).toBe('001');
     expect(employee.email).toBe('erik@noemail.com');
+});
+
+
+//-- Getting name
+test('GET employee.name value', () => {
+    
+    //-- make employee to test
+    const employee = new Employee("Erik",'001',"erik@noemail.com");
+
+    expect(employee.getName()).toBe("Erik");
+
+});
+
+
+//-- Getting ID
+test('GET employee.id value', () => {
+
+    //-- make employee to test
+    const employee = new Employee("Erik",'001',"erik@noemail.com");
+
+    expect(employee.getID()).toBe("001");
+});
+
+
+//-- Getting email
+test('GET employee.email value', () => {
+
+    //-- make employee to test
+    const employee = new Employee("Erik",'001',"erik@noemail.com");
+
+    expect(employee.getEmail()).toBe("erik@noemail.com");
 });
